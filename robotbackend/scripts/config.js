c86bcc69acb34d54ad386a7e12d0d20dd713f63d
Stella MAO <stellamaoyx@gmail.com>
2025-11-21 19:24:23 -0800

    revised firebase api

--- robotbackend/scripts/config.js
+++ robotbackend/scripts/config.js
@@ -1,12 +1,13 @@
 // Default Firebase project (override by defining window.EMAR_FIREBASE_CONFIG before this file loads)
-const DEFAULT_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyB10dhQqD1TMXTJfQNLMmFkrJtVyQ4JtUA",
+const firebaseConfig = {
+  apiKey: "AIzaSyB10dHhQqD1TMXTJfQNLmFkrJtVyQ4JTuA",
   authDomain: "flexi-f7d77.firebaseapp.com",
   databaseURL: "https://flexi-f7d77-default-rtdb.firebaseio.com",
   projectId: "flexi-f7d77",
-  storageBucket: "flexi-f7d77.appspot.com",
+  storageBucket: "flexi-f7d77.firebasestorage.app",
   messagingSenderId: "441373455093",
-  appId: "1:441373455093:web:038d10e9960e0e08b14bbc"
+  appId: "1:441373455093:web:038d10e9960e0e08b14bbc",
+  measurementId: "G-NNWWSD5BCH"
 };
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
